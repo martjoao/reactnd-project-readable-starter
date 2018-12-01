ad3d6c1bc2ce2593990e889d7eab83669de917c9
João Martins <martins.j.neto@gmail.com>
2018-12-01 00:37:04 -0300

    Setup eslint

--- readable/src/App.js
+++ readable/src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Oi
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    Oi
+  </div>
+);
 
 export default App;
