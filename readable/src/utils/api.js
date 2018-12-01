553ce48c70679ab3680ca5f01b9767f54db137fc
João Martins <martins.j.neto@gmail.com>
2018-12-01 02:10:40 -0300

    Update API client to include headers

--- readable/src/utils/api.js
+++ readable/src/utils/api.js
@@ -1,46 +1,61 @@
-import axios from 'axios';
+import AxiosLib from 'axios';
 
 const SERVER_URL = 'http://localhost:3001';
+const AUTHORIZATION_TOKEN = 'REACTND_RANDOM_AUTH_TOKEN_HERE';
+
+const axios = AxiosLib.create({
+  baseURL: SERVER_URL,
+  timeout: 10000,
+});
+
+axios.interceptors.request.use(
+  (config) => {
+    const newConfig = { ...config };
+    newConfig.headers.authorization = AUTHORIZATION_TOKEN;
+    return newConfig;
+  },
+  error => Promise.reject(error),
+);
 
 export const getCategories = () => (
-  axios.get(`${SERVER_URL}/categories`));
+  axios.get('/categories'));
 
 export const getPosts = (category) => {
   if (category) {
-    return axios.get(`${SERVER_URL}/categories/${category}/posts`);
+    return axios.get(`/categories/${category}/posts`);
   }
-  return axios.get(`${SERVER_URL}/posts`);
+  return axios.get('/posts');
 };
 
 export const addPost = post => (
-  axios.post(`${SERVER_URL}/posts`, post));
+  axios.post('/posts', post));
 
 export const getPost = postId => (
-  axios.get(`${SERVER_URL}/posts/${postId}`));
+  axios.get(`/posts/${postId}`));
 
 export const votePost = (postId, option) => (
-  axios.post(`${SERVER_URL}/posts/${postId}`, { option }));
+  axios.post(`/posts/${postId}`, { option }));
 
 export const editPost = (postId, postDetails) => (
-  axios.put(`${SERVER_URL}/posts/${postId}`, postDetails));
+  axios.put(`/posts/${postId}`, postDetails));
 
 export const deletePost = postId => (
-  axios.delete(`${SERVER_URL}/posts/${postId}`));
+  axios.delete(`/posts/${postId}`));
 
 export const getPostComments = postId => (
-  axios.get(`${SERVER_URL}/posts/${postId}/comments`));
+  axios.get(`/posts/${postId}/comments`));
 
 export const addComment = comment => (
-  axios.post(`${SERVER_URL}/comments`, comment));
+  axios.post('/comments', comment));
 
 export const getComment = commentId => (
-  axios.get(`${SERVER_URL}/comments/${commentId}`));
+  axios.get(`/comments/${commentId}`));
 
 export const voteComment = (commentId, option) => (
-  axios.post(`${SERVER_URL}/comments/${commentId}`, { option }));
+  axios.post(`/comments/${commentId}`, { option }));
 
 export const editComment = (commentId, commentDetails) => (
-  axios.put(`${SERVER_URL}/comments/${commentId}`, commentDetails));
+  axios.put(`/comments/${commentId}`, commentDetails));
 
 export const deleteComment = commentId => (
-  axios.delete(`${SERVER_URL}/comments/${commentId}`));
+  axios.delete(`/comments/${commentId}`));
